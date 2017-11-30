0ff7c2769cd09a0410801d59a85b04d117ac07bd
Poornima <poornima.selvam@thomsonreuters.com>
2017-11-30 09:29:43 +0530

    Memento Pattern

--- JavaDesignPatterns/src/behavioral/mementoPattern/test/MementoPatternDemo.java
+++ JavaDesignPatterns/src/behavioral/mementoPattern/test/MementoPatternDemo.java
@@ -3,6 +3,14 @@ package behavioral.mementoPattern.test;
 import behavioral.mementoPattern.model.CareTaker;
 import behavioral.mementoPattern.model.Originator;
 
+/**
+ * Memento pattern is used to restore state of an object to a previous state.
+ * 
+ *  Memento pattern uses three actor classes. Memento contains state of an object to be restored. 
+ *  Originator creates and stores states in Memento objects and Caretaker object is responsible to restore object state from Memento.
+ *  We have created classes Memento, Originator and CareTaker.
+ *
+ */
 public class MementoPatternDemo {
 	public static void main(String[] args) {
 		Originator originator = new Originator();
